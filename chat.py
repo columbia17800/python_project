be6a99b6eb3029f5e75eb5caaaf74da56e991756
j24zhan <j24zhan@uwaterloo.ca>
2020-04-21 17:35:13 -0400

    chat.py

---
+++ chat.py
@@ -0,0 +1,10 @@
+class Chat():
+	""A simple TCP/UDP chat""
+	
+	def __init__(self, targetIP, targetPort, selfIP, selfPort):
+		self.targetIP = targetIP
+		self.targetPort = targetPort
+		self.selfIP = selfIP
+		self.selfPort = self.selfPort
+
+

23b165c624a155c5759fa3fc897ac31dfbff8727
j24zhan <j24zhan@uwaterloo.ca>
2020-04-22 17:46:55 -0400

    added some files to work on

--- chatui.py
+++ chatui.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-import PySimepleGUI as sg
 from settings import UISetting
 
 class ChatUI():
-	def __init__(UISetting set):
-		
+	def __init__(self, set: UISetting):
 		
--- uisetting.py
+++ uisetting.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import PySimepleGUI as sg
+from typing import List,Dict,Tuple,Any
 
 class UISetting():
 	def __init__(self):
@@ -6,4 +8,17 @@ class UISetting():
 		self.window_font = 'Helvetica'
 		self.window_font_size = ' 12'
 		self.button_size = (8,2)
-		self.
+		self.text_words = 'Your output will enter here'
+		self.text_size = (40,1)
+		self.output_size = (100, 20)
+		self.output_font = ('Helvetica 10')
+		self.multiline_size = (70, 5)
+		self.multiline_enter_submits = True
+		self.multiline_key = '-QUERY-'
+		self.do_not_clear = False
+		self.Buttons = []					#list of Buttons(tuples)
+	def settupButton(type: str, color: Tuple, bind_return: bool):
+		# append new Button to list
+		self.Buttons.append(
+			sg.Button(type, button_color=color, bind_return_key=bind_return)
+			)

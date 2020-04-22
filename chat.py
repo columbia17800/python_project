f170bcbee634e05235ec993c443895770fe68ed7
j24zhan <j24zhan@uwaterloo.ca>
2020-04-22 09:10:59 -0400

    update chat.py

--- chat.py
+++ chat.py
@@ -1,4 +1,6 @@
-class Chat():
+import socket
+
+class Chatter():
 	""A simple TCP/UDP chat""
 	
 	def __init__(self, targetIP, targetPort, selfIP, selfPort):
@@ -6,5 +8,47 @@ class Chat():
 		self.targetPort = targetPort
 		self.selfIP = selfIP
 		self.selfPort = self.selfPort
+		self.blockTime = 1
+		self.sock = socket.socket(
+			socket.AF_INET, 
+			socket.SOCK_STREAM)
+
+	def __del__(self):
+		closeSocket()
+		
+
+	def initialTunnel(self):
+		# local reference
+		# following only accept IPv4
+		s = self.sock
+		s.bind( ( self.selfIP, self.selfPort ) )
+		s.listen( self.blockTime )
+		# accept calling
+		conn, addr = s.accept()
+
+		dataList = []
+		while True:
+			data = conn.rev(1024)
+			if not data: break
+			dataList.append[data]
+		
+		# channel ends
+		conn.close()
+		# might return dataList
 
 
+	def callServer(self):
+		s = self.socket
+		s.connect( ( self.targetIP, self.targetPort ) )
+		
+		while True:						# break if 
+			s.send( self.data )
+			recv = s.recv(1024)			# receive stored at local
+			if str(recv) = "¿" : break
+			print repr(recv)
+		
+		closeSocket(self)						# close socket
+
+
+	def closeSocket(self):
+		s.close()

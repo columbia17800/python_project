0998d5d18cb3a448092d0e3d7a353825599a040c
z4lyu <z4lyu@edu.uwaterloo.ca>
2020-04-27 19:01:10 -0400

    Improve UI profiles.

--- listui.py
+++ listui.py
@@ -4,5 +4,29 @@ import PySimpleGUI as sg
 import time
 
 class ListUI():
-	def __init__(self, set: UISetting):
-		raise NotImplementedError
+	def __init__(self, set: UISetting, namelist):
+		layout = [[sg.Text('List of users')],
+			[sg.Input(size = (20, 1), enable_events = True, key = '-INPUT-')],
+			[sg.Listbox(namelist, size = (20, 4), key = '-LIST-')],
+			[sg.Button('Chat!'), sg.Button('Exit')]]
+	
+	window = sg.Window('Choose someone to chat!', layout)
+
+	while True:
+		event, values = window.read()
+		if event in (None, 'Exit'):
+			break
+		if values['-INPUT-'] != '':
+			wanted = values['-INPUT-']
+			targets = [x for x in namelist if search in x]
+			window['-LIST-'].update(targets)
+		else:
+			window['-LIST-'].update(namelist)
+		if event == 'Chat!':
+			that = values['-LIST-'][0]
+			that = that + '!'
+			print("Start chating with", that)
+			# Start chating here
+
+	def __del__(self):
+		window.close()
--- loginui.py
+++ loginui.py
@@ -5,4 +5,9 @@ import time
 
 class LogUI():
 	def __init__(self, set: UISetting):
-		raise NotImplementedError
+		sg.theme('DarkAmber')
+		id = sg.popup_get_text('Type in a cool =ID here')
+		# if ID exist then return FAIL
+		if id == '':
+			print('Null ID!')
+		# else return id to the server

c3c1d743228cb7570e69962eee88ce3edeaab8d9
j24zhan <j24zhan@uwaterloo.ca>
2020-05-23 00:03:27 -0400

    comments the methods of client

--- src/loginui.py
+++ src/loginui.py
@@ -8,6 +8,18 @@ except:
 import PySimpleGUI as sg
 import time
 
+'''
+methods in client:
+
+请求链接服务器（登录：namepair里面是用户名和密码，用户名在前
+connect_to_server(namepair: Tuple[str, str]):				call to connect to server
+注册：namepair要不是单独用户名，就是用户名和密码
+	单独用户名只会raise,但可以检测出用户名是否重复
+register(namepair: Tuple[str,] or Tuple[str, str]):			register this user into server
+捡朋友：从listui里选id作为friendname，请求p2p连接
+pick_a_friend(friendname: str):					pick a friend in friend list that is about to call on
+'''
+
 class LogUI():
 	def __init__(self):
 		sg.theme('DarkAmber')
